64ad699d055bcda67ac73863358994c6f71c2a8d
David_Larios <david.larios.ext@terpel.com>
2025-05-28 09:37:34 -0500

    feat: add example validation routes with ozzo

--- presentation/routers/configuration.go
+++ presentation/routers/configuration.go
@@ -2,9 +2,16 @@ package routers
 
 import (
 	"genesis/pos/presentation/handlers"
+	"genesis/pos/presentation/middleware"
+	"genesis/pos/presentation/schema"
 	"github.com/gin-gonic/gin"
 )
 
 func CurrentTime(router *gin.RouterGroup) {
-	router.GET("/", handlers.GetCurrentTime)
+	// example
+	router.POST("/:id",
+		middleware.ValidateParams(new(schema.CurrentTimeParams)),
+		middleware.ValidateQuery(new(schema.CurrentTimeQuery)),
+		middleware.ValidateBody(new(schema.CurrentTimeBody)),
+		handlers.GetCurrentTime)
 }
